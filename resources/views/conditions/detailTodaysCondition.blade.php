fef06da856850a1a2259688cc7adbd866e6e3ea1
N0S0 <snct.h23b09@gmail.com>
2021-02-15 12:12:29 +0900

    modify todayscondition-view

--- resources/views/conditions/detailTodaysCondition.blade.php
+++ resources/views/conditions/detailTodaysCondition.blade.php
@@ -8,25 +8,45 @@
       <div class="card">
         <div class="card-header">Today's Condition</div>
         <div class="card-body">
-          <span for="date">日付：</span>{{$condition[0]->date->format('Y-m-d')}}
-          <br><span for="taion">体温：</span>@if(isset($condition[0]->taion)){{$condition[0]->taion}}@endif
-          <ul class="condition-check">
-            <li>症状：</li>
-            <li>@if (strpos($condition[0]->condition, '1') !== false)
-              味覚異常
-              @endif
-              @if (strpos($condition[0]->condition, '2') !== false)
-              /嗅覚異常
-              @endif
-              @if (strpos($condition[0]->condition, '3') !== false)
-              /咳
-              @endif
-              @if (strpos($condition[0]->condition, '4') !== false)
-              /倦怠感
-              @endif
-            </li>
-          </ul>
-          コメント:@if(isset($condition[0]->comment))
+          <span for="date">日付：</span>{{$condition[0]->date->format('Y-m-d(D)')}}
+          <br><span for="taion">体温：</span>@if(isset($condition[0]->taion)){{$condition[0]->taion}}@endif℃
+          {{-- <ul class="condition-check"> --}}
+          <br>症状：<?php $text = ''; ?>
+          @if (strpos($condition[0]->condition, '1') !== false)
+          <?php 
+                $text .= '味覚異常';
+                ?>
+          @endif
+          @if (strpos($condition[0]->condition, '2') !== false)
+          <?php
+                if ($text == ''){
+                $text .= '嗅覚異常';
+                }else{
+                $text .= '/嗅覚異常';
+                }
+                ?>
+          @endif
+          @if (strpos($condition[0]->condition, '3') !== false)
+          <?php
+                if ($text == ''){
+                $text .= '咳・痰';
+                }else{
+                $text .= '/咳・痰';
+                }
+                ?>
+          @endif
+          @if (strpos($condition[0]->condition, '4') !== false)
+          <?php
+                if ($text == ''){
+                $text .= '倦怠感';
+                }else{
+                $text .= '/倦怠感';
+                }
+                ?>
+          @endif
+          {{$text}}
+
+          <br>コメント:@if(isset($condition[0]->comment))
           {{$condition[0]->comment}}
           @endif
         </div><!-- end of card-body -->
